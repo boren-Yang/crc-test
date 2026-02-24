542195a10d9c1ca313b0e603f69dd1fd2495f448
boren <boren@toolsbiotech.com>
2026-02-24 13:11:15 +0800

    Add Speed Insights package

--- website1/app/page.tsx
+++ website1/app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
       {/* 只有在手機版顯示的區塊 (md 以上就隱藏) */}
       <div className="block md:hidden bg-blue-100 p-4">
         <h1>這是專屬手機版的標題</h1>
-        <img src="/mobile-hero.png" alt="mobile" className="w-full" />
+        <img src="bg1.png" alt="mobile" className="w-full" />
       </div>
 
       {/* 只有在電腦版顯示的區塊 (預設隱藏，md 以上才變 flex 或 block) */}
       <div className="hidden md:block bg-green-100 p-10">
         <h1>這是專屬電腦版的超大橫幅</h1>
         <div className="h-[700px]">
-           <img src="/desktop-bg.png" className="w-full h-full object-cover" />
+           <img src="/desktbg.png" className="w-full h-full object-cover" />
         </div>
       </div>
     </main>
