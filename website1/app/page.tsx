38948adcbc46d7c8ec691cb331f22b751bc26933
boren <boren@toolsbiotech.com>
2026-02-25 11:20:34 +0800

    Add Speed Insights package

--- website1/app/page.tsx
+++ website1/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
         <h1>這是專屬手機版的標題</h1>
         <img src="bg1.png" alt="mobile" className="w-full" />
       </div>
-
+<div className="bg-red-500 text-white p-10 text-3xl">
+  如果背景是紅色，代表 Tailwind 成功了！
+</div>
       {/* 只有在電腦版顯示的區塊 (預設隱藏，md 以上才變 flex 或 block) */}
       <div className="hidden md:block bg-green-100 p-10">
         <h1>這是專屬電腦版的超大橫幅</h1>
